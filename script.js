2c118f2118cf3e7e3111e9863ef00ef0fcd1b92c
Michał Wodniczak <michal.wodniczak@o2.pl>
2023-01-16 16:31:34 +0100

    added newTask and show the tasks

--- script.js
+++ script.js
@@ -1,8 +1,34 @@
 {
-    const init = () =>{
+    const tasks = [];
+
+    const addNewTask = () => {
+        tasks.push({
+            content: document.querySelector(".js-task").value.trim()
+        })
+        render();
+    }
+
+    const render = () => {
+        let htmlString = "";
+
+        for (const task of tasks) {
+            htmlString += `
+            <li class="section__listItem"> 
+                <button>✔</button>
+                ${task.content}
+                <button>🗑️</button>
+            </li>
+            `;
+        }
+        document.querySelector(".js-tasks").innerHTML = htmlString
+    }
+
+    const init = () => {
+        render();
         const form = document.querySelector(".js-form");
-        form.addEventListener("submit", (event) =>{
+        form.addEventListener("submit", (event) => {
             event.preventDefault();
+            addNewTask();
         })
     }
     init();
